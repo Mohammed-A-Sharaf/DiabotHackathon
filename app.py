0dedf080e0f183eeb1d97a6c3bf9481c31d8099b
Mohammed-A-Sharaf <168541121+Mohammed-A-Sharaf@users.noreply.github.com>
2025-09-21 14:32:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -234,8 +234,6 @@ st.set_page_config(
 # Sidebar navigation
 with st.sidebar:
     st.title("DiaBot AI")
-    st.markdown("---")
-
     st.image("DIaBot Logo/logo.png", width=200)
     st.markdown("---")
     
